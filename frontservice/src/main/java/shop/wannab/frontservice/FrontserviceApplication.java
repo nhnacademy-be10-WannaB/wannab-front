0b6cd9fffc754d8628c9551c634c677eb3003e83
chosun-nhn21 <yud01063@gmail.com>
2025-06-11 09:40:40 +0900

    feat/프론트 서버 생성

---
+++ frontservice/src/main/java/shop/wannab/frontservice/FrontserviceApplication.java
@@ -0,0 +1,13 @@
+package shop.wannab.frontservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontserviceApplication.class, args);
+	}
+
+}

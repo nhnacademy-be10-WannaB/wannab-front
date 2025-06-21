55304ea77088b5818c7cbe68516c2185bef63cb1
LimJaeEun0930 <je9513666@gmail.com>
2025-06-21 20:07:52 +0900

    feat: cart controller 구현

--- src/main/java/shop/wannab/frontservice/order/client/OrderApiClient.java
+++ src/main/java/shop/wannab/frontservice/order/client/OrderApiClient.java
@@ -1,15 +1,30 @@
 package shop.wannab.frontservice.order.client;
 
+import jakarta.servlet.http.Cookie;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
+import shop.wannab.frontservice.order.dto.OrderBookInfoListDto;
 import shop.wannab.frontservice.order.dto.OrderItemListDto;
 import shop.wannab.frontservice.order.dto.OrderPageRequestDto;
 
-@FeignClient(name = "order-payment-service", url = "${order-payment.api.url}")
+@FeignClient(name = "order-payment-service")
 public interface OrderApiClient {
+
+    @PostMapping
+    Cookie createCart(@RequestHeader(value = "X-User-Id", required = false) Long userIdentifier);
 
     @PostMapping("/api/orders")
     OrderPageRequestDto getNecesaryOrderInfo(@RequestHeader("X-User-Id") Long userId, @RequestBody OrderItemListDto orderItemListDto);
+
+    @GetMapping("/api/cart")
+    OrderBookInfoListDto getCartItems(@RequestHeader("X-User-Id") Long userId);
+
+    @PostMapping("/api/cart/books")
+    OrderBookInfoListDto addProductToCart(@RequestHeader("X-User-Id") Long userId, @RequestParam Long bookId);
+
+    @PutMapping("/api/cart/books/{book-id}")
+    OrderBookInfoListDto updateCartItemQuantity(@RequestHeader("X-User-Id") Long userId, @PathVariable(name = "book-id") Long bookId, @RequestParam int quantity);
+
+    @DeleteMapping("/api/cart/books/{book-id}")
+    OrderBookInfoListDto removeProductFromCart(@RequestHeader("X-User-Id") Long userId, @PathVariable(name = "book-id") Long bookId);
 }
---
+++ src/main/java/shop/wannab/frontservice/order/controller/CartController.java
@@ -0,0 +1,59 @@
+package shop.wannab.frontservice.order.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import shop.wannab.frontservice.order.client.OrderApiClient;
+import shop.wannab.frontservice.order.dto.OrderBookInfoListDto;
+
+import java.util.List;
+import java.util.Objects;
+
+@Controller
+@RequestMapping("/user/main-cart")
+@RequiredArgsConstructor
+public class CartController {
+    private final OrderApiClient orderApiClient;
+
+    @GetMapping
+    public String getCartPage(@RequestHeader("X-USER-ID") Long userId, Model model) {
+        if (Objects.isNull(userId)) {//비회원 && 장바구니에 아무것도 담지 않을시
+            OrderBookInfoListDto emptyCart = new OrderBookInfoListDto(List.of());
+            model.addAttribute("cartItems", emptyCart);
+            return "user/main-cart";
+        }
+        OrderBookInfoListDto cartItems = orderApiClient.getCartItems(userId);
+        model.addAttribute("cartItems", cartItems.getOrderBookInfos());
+        return "user/main-cart";
+    }
+
+    @PostMapping("/books")
+    public String addItemToCart(@RequestHeader(value = "X-USER-ID", required = false) Long userId, @RequestParam Long bookId, HttpServletResponse response) {
+        if (Objects.isNull(userId)) {//비회원 && 장바구니에 처음 상품 담을시
+            Cookie guestIdentifier = orderApiClient.createCart(null);
+            response.addCookie(guestIdentifier);
+            userId = Long.valueOf(guestIdentifier.getValue());
+        }
+        orderApiClient.addProductToCart(userId, bookId);
+        return "redirect:/user/main-cart";
+    }
+
+    @PutMapping("/books/{book-id}")
+    public String updateCartItemQuantity(@RequestHeader("X-User-Id") Long userId, @PathVariable(name = "book-id") Long bookId, @RequestParam int quantity) {
+        if (Objects.nonNull(userId)) {
+            orderApiClient.updateCartItemQuantity(userId, bookId, quantity);
+        }
+        return "redirect:/user/main-cart";
+    }
+
+    @DeleteMapping("/books/{book-id}")
+    public String removeCartItem(@RequestHeader("X-USER-ID") Long userId, @PathVariable(name = "book-id") Long bookId) {
+        if (Objects.nonNull(userId)) {
+            orderApiClient.removeProductFromCart(userId, bookId);
+        }
+        return "redirect:/user/main-cart";
+    }
+}

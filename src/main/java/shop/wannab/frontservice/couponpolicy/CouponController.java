48923f5fd9f9d81098a5a20553b6776d10d7ebce
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-06-27 13:42:44 +0900

    feat: 쿠폰 정책 기능

--- src/main/java/shop/wannab/frontservice/couponpolicy/CouponController.java
+++ src/main/java/shop/wannab/frontservice/couponpolicy/CouponController.java
@@ -4,8 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -25,8 +27,11 @@ public class CouponController {
         model.addAttribute("currentUri", request.getRequestURI());
         //겟요청으로 쿠폰리스트
         //모델에 넣기
+        List<CategoryHierarchyDto> categoryHierarchy = couponApiClient.getCategoryHierarchy();
         List<CouponPolicyDto> couponPolicies = couponApiClient.getCouponPolicies();
         CouponPolicyCreateDto couponPolicyCreateDto = new CouponPolicyCreateDto();
+
+        model.addAttribute("categoryHierarchy", categoryHierarchy); // 계층 구조 데이터
         model.addAttribute("couponPolicyCreateDto",couponPolicyCreateDto);
         model.addAttribute("couponPolicies", couponPolicies);
         return "admin/coupon";
@@ -41,6 +46,18 @@ public class CouponController {
             redirectAttributes.addFlashAttribute("successMessage", "쿠폰 정책이 성공적으로 등록되었습니다.");
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("errorMessage", "쿠폰 정책 등록에 실패했습니다: " + e.getMessage());
+        }
+        return "redirect:/admin/coupon";
+    }
+
+    @DeleteMapping("/coupons/{couponPolicyId}")
+    public String deleteCouponPolicy(@PathVariable Long couponPolicyId
+            , RedirectAttributes redirectAttributes) {
+        try{
+            couponApiClient.deleteCouponPolicy(couponPolicyId);
+            redirectAttributes.addFlashAttribute("successMessage","쿠폰 정책이 성공적으로 삭제되었습니다.");
+        }catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage","쿠폰 정책 등록에 실패했습니다.: " + e.getMessage());
         }
         return "redirect:/admin/coupon";
     }

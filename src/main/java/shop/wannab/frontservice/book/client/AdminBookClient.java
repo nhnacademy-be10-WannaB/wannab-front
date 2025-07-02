de4d2854192f43863091b662c277956cc97b0e9d
gnsals0904 <gnsals0904@naver.com>
2025-07-02 09:44:42 +0900

    feat: 알라딘 도서 등록 기능 구현

--- src/main/java/shop/wannab/frontservice/book/client/AdminBookClient.java
+++ src/main/java/shop/wannab/frontservice/book/client/AdminBookClient.java
@@ -2,9 +2,11 @@ package shop.wannab.frontservice.book.client;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import shop.wannab.frontservice.book.client.request.SearchRequest;
 import shop.wannab.frontservice.book.client.response.SearchResponse;
+import shop.wannab.frontservice.book.controller.request.AladinBookRequest;
 
 /**
  * Book Service 에 요청을 보내는 FeignClient
@@ -16,5 +18,8 @@ public interface AdminBookClient {
 
     @PostMapping("/api/admin/aladin/books/search")
     SearchResponse searchFromBookService(SearchRequest request);
+
+    @PostMapping("/api/admin/aladin/books")
+    ResponseEntity<Void> createdAladinBook(AladinBookRequest request);
 
 }
--- src/main/java/shop/wannab/frontservice/book/service/AdminBookService.java
+++ src/main/java/shop/wannab/frontservice/book/service/AdminBookService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import shop.wannab.frontservice.book.client.AdminBookClient;
 import shop.wannab.frontservice.book.client.request.SearchRequest;
 import shop.wannab.frontservice.book.client.response.SearchResponse;
+import shop.wannab.frontservice.book.controller.request.AladinBookRequest;
 import shop.wannab.frontservice.book.controller.response.SearchBookResponse;
 
 @Service
@@ -16,5 +17,9 @@ public class AdminBookService {
     public SearchBookResponse searchBooks(SearchRequest searchRequest) {
         SearchResponse searchResponse = adminBookClient.searchFromBookService(searchRequest);
         return SearchBookResponse.from(searchResponse);
+    }
+
+    public void registerAladinBook(AladinBookRequest request){
+        adminBookClient.createdAladinBook(request);
     }
 }

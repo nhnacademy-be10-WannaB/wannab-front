ce5419c60a3d481059e88d10a99986a066163e66
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-07-06 18:11:54 +0900

    refactor: 카테고리 API 호출 로직을 CategoryClient로 분리

--- src/main/java/shop/wannab/frontservice/couponpolicy/CouponPageDataDto.java
+++ src/main/java/shop/wannab/frontservice/couponpolicy/CouponPageDataDto.java
@@ -3,6 +3,7 @@ package shop.wannab.frontservice.couponpolicy;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import shop.wannab.frontservice.category.controller.response.CategoryHierarchyDto;
 
 @Getter
 @Setter

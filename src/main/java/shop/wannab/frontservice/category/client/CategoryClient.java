4e2a1ec572b24f10da751ac998cd1ca8ee369be8
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-07-06 18:12:56 +0900

    refactor: 카테고리 API 호출 로직을 CategoryClient로 분리

---
+++ src/main/java/shop/wannab/frontservice/category/client/CategoryClient.java
@@ -0,0 +1,15 @@
+package shop.wannab.frontservice.category.client;
+
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import shop.wannab.frontservice.category.controller.response.CategoryHierarchyDto;
+import shop.wannab.frontservice.category.controller.response.ParentCategoryDto;
+
+@FeignClient(name = "gateway", url = "${gateway.api.url}", path = "/book-service", contextId = "categoryClient")
+public interface CategoryClient {
+
+    @GetMapping("/api/categories/hierarchy")
+    List<CategoryHierarchyDto> getCategoryHierarchy();
+
+}

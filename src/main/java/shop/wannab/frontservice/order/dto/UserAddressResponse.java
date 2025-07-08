f34e181db1a1ee31fd2efddae7a6e52100e81cb7
LimJaeEun0930 <je9513666@gmail.com>
2025-07-08 15:14:19 +0900

    fix: 역직렬화시 필요한 어노테이션 추가

--- src/main/java/shop/wannab/frontservice/order/dto/UserAddressResponse.java
+++ src/main/java/shop/wannab/frontservice/order/dto/UserAddressResponse.java
@@ -1,8 +1,10 @@
 package shop.wannab.frontservice.order.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class UserAddressResponse {
     private Long addressId;
     private String addressName;

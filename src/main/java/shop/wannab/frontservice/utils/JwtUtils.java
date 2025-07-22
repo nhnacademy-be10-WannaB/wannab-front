f1f2b9cdbdf549ef918a2aa3013f8930f166b7db
gnsals0904 <gnsals0904@naver.com>
2025-07-22 17:20:21 +0900

    fix: jwt 로깅 추가

--- src/main/java/shop/wannab/frontservice/utils/JwtUtils.java
+++ src/main/java/shop/wannab/frontservice/utils/JwtUtils.java
@@ -6,9 +6,11 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class JwtUtils {
 
@@ -16,11 +18,19 @@ public class JwtUtils {
     private String secretKey;
 
     public Claims parse(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        log.warn("jwt key : {}", secretKey);
+        log.warn("signing key : {}", Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)));
+        Claims body = null;
+        try {
+            body = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e){
+            log.warn("JwtUtils, parse error : {}", e.getMessage());
+        }
+        return body;
     }
 
     public boolean isExpired(String token) {
